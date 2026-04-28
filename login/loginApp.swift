66cd51200ca7074bb52541435eb345779ddb27bb
giovanaassis0101 <giovanaassisstos@gmail.com>
2026-04-28 17:53:33 -0300

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Giovana Assis on 28/04/26.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ loginTests/loginTests.swift
@@ -0,0 +1,17 @@
+//
+//  loginTests.swift
+//  loginTests
+//
+//  Created by Giovana Assis on 28/04/26.
+//
+
+import Testing
+@testable import login
+
+struct loginTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
